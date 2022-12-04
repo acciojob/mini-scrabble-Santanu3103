1273b9c5687cb8e336e3a80063d83c07caf54346
santunu <santanuroy253396@gmail.com>
2022-12-04 23:54:40 +0530

    FIRST COMMIT

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+function countWord() {
+    let sentence = document.getElementById("evaluatedText").value;
+   
+    let currentValue=0;
+    let arrayOfWord = sentence.split(" ");
+    arrayOfWord.forEach(word => {
+            if(word!==' '){
+                currentValue =  word.length;
+                console.log(currentValue); 
+            }
+          
+    });
+    
+    
+    let display = document.getElementById("letterCount");
+    display.innerHTML = 'Letter Count: '+ currentValue;
+}
+
