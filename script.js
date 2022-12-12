4dede044554e6f56cba1b56b0d2089f4a8f0e29f
santunu <santanuroy253396@gmail.com>
2022-12-12 18:36:17 +0530

    fifth comit

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function countWord() {
     let sentence = document.getElementById("evaluatedText").value;
    
     let currentValue=0;
-    let array= sentence.split("");
-        currentValue = array.length;
-    
+    let array= sentence.replace(' ','').split("");
+       currentValue = array.length;
+      
+    console.log(currentValue);
     
     let display = document.getElementById("letterCount");
     display.innerHTML = 'Letter Count: '+ currentValue;
